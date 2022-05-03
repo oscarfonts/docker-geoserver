1a1121317a060a43ac760c1859cb52726c8df4ab
Oscar Fonts <oscar.fonts@geomati.co>
2022-05-03 10:25:29 +0200

    Start script enhancements by @guydriesen fixes #31

--- 2.20.4/start.sh
+++ 2.20.4/start.sh
@@ -1,23 +1,32 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ -n "${CUSTOM_UID}" ];then
+if [ -n "${CUSTOM_UID}" ]; then
   echo "Using custom UID ${CUSTOM_UID}."
   usermod -u ${CUSTOM_UID} tomcat
-  find / -user 1099 -exec chown -h tomcat {} \; 
+  find / -xdev -user 1099 -exec chown -h tomcat '{}' +
 fi
 
-if [ -n "${CUSTOM_GID}" ];then
+if [ -n "${CUSTOM_GID}" ]; then
   echo "Using custom GID ${CUSTOM_GID}."
   groupmod -g ${CUSTOM_GID} tomcat
-  find / -group 1099 -exec chgrp -h tomcat {} \;
+  find / -xdev -group 1099 -exec chgrp -h tomcat '{}' +
 fi
 
-#We need this line to ensure that data has the correct rights
-chown -R tomcat:tomcat ${GEOSERVER_DATA_DIR} 
-chown -R tomcat:tomcat ${GEOSERVER_EXT_DIR}
+# We need this line to ensure that data has the correct rights
+if [ "$(stat -c %U:%G ${GEOSERVER_DATA_DIR})" != "tomcat:tomcat" ]; then
+  chown -R tomcat:tomcat "${GEOSERVER_DATA_DIR}"
+fi
 
-for ext in `ls -d "${GEOSERVER_EXT_DIR}"/*/`; do
-  su tomcat -c "cp "${ext}"*.jar /usr/local/geoserver/WEB-INF/lib"
-done
+# Install extensions
+find "${GEOSERVER_EXT_DIR}" -mindepth 2 -maxdepth 2 -type f -iname '*.jar' \
+ -exec install -o tomcat -g tomcat -p '{}' /usr/local/geoserver/WEB-INF/lib \;
 
-su tomcat -c "/usr/local/tomcat/bin/catalina.sh run"
+# https://unix.stackexchange.com/questions/132663/how-do-i-drop-root-privileges-in-shell-scripts
+# http://jdebp.info/FGA/dont-abuse-su-for-dropping-privileges.html
+# --inh-caps=-all results in an error on some systems (setpriv: libcap-ng is too old for "all" caps)
+# get a list of all capabilities, prefix with '-', make a one line ',' sep list & remove the last ',':
+all_caps=$(setpriv --list-caps | sed -e 's/^/-/' | tr '\n' ',' | sed -e 's/,$//')
+command="setpriv --reuid=tomcat --regid=tomcat --init-groups --inh-caps=${all_caps}"
+command="${command} /usr/local/tomcat/bin/catalina.sh run"
+
+exec ${command}
