277833f34171fb2ee3614094e4f0d5bc3fcbe665
Francisco Pérez Sampayo <francisco.perez@geomatico.es>
2025-03-06 13:47:51 +0100

    Configures ubuntu user as default (#51)
    * Configures ubuntu user as default
    
    * Same changes to other versiones

--- 2.23.6/start.sh
+++ 2.23.6/start.sh
@@ -27,31 +27,41 @@ fi
 
 if [ -n "${CUSTOM_UID}" ]; then
   echo "Using custom UID ${CUSTOM_UID}."
-  usermod -u ${CUSTOM_UID} tomcat
-  find / -xdev -user 1099 -exec chown -h tomcat '{}' +
+  UBUNTU_UID=$(id -u "ubuntu" 2>/dev/null)
+  if [ "$UBUNTU_UID" -eq "$CUSTOM_UID" ]; then
+    echo "CUSTOM_UID already in use for ubuntu user. Nothing to do."
+  else
+    usermod -u ${CUSTOM_UID} ubuntu
+    find / -xdev -user 1000 -exec chown -h ubuntu '{}' +
+  fi
 fi
 
 if [ -n "${CUSTOM_GID}" ]; then
   echo "Using custom GID ${CUSTOM_GID}."
-  groupmod -g ${CUSTOM_GID} tomcat
-  find / -xdev -group 1099 -exec chgrp -h tomcat '{}' +
+  UBUNTU_GID=$(id -g "ubuntu" 2>/dev/null)
+  if [ "$UBUNTU_GID" -eq "$CUSTOM_GID" ]; then
+    echo "CUSTOM_GID already in use for ubuntu user. Nothing to do."
+  else
+    groupmod -g ${CUSTOM_GID} ubuntu
+    find / -xdev -group 1000 -exec chgrp -h ubuntu '{}' +
+  fi
 fi
 
 # We need this line to ensure that data has the correct rights
-if [ "$(stat -c %U:%G ${GEOSERVER_DATA_DIR})" != "tomcat:tomcat" ]; then
-  chown -R tomcat:tomcat "${GEOSERVER_DATA_DIR}"
+if [ "$(stat -c %U:%G ${GEOSERVER_DATA_DIR})" != "ubuntu:ubuntu" ]; then
+  chown -R ubuntu:ubuntu "${GEOSERVER_DATA_DIR}"
 fi
 
 # Install extensions
 find "${GEOSERVER_EXT_DIR}" -mindepth 2 -maxdepth 2 -type f -iname '*.jar' \
- -exec install -o tomcat -g tomcat -p '{}' /usr/local/geoserver/WEB-INF/lib \;
+ -exec install -o ubuntu -g ubuntu -p '{}' /usr/local/geoserver/WEB-INF/lib \;
 
 # https://unix.stackexchange.com/questions/132663/how-do-i-drop-root-privileges-in-shell-scripts
 # http://jdebp.info/FGA/dont-abuse-su-for-dropping-privileges.html
 # --inh-caps=-all results in an error on some systems (setpriv: libcap-ng is too old for "all" caps)
 # get a list of all capabilities, prefix with '-', make a one line ',' sep list & remove the last ',':
 all_caps=$(setpriv --list-caps | sed -e 's/^/-/' | tr '\n' ',' | sed -e 's/,$//')
-command="setpriv --reuid=tomcat --regid=tomcat --init-groups --inh-caps=${all_caps}"
+command="setpriv --reuid=ubuntu --regid=ubuntu --init-groups --inh-caps=${all_caps}"
 command="${command} /usr/local/tomcat/bin/catalina.sh run"
 
 exec ${command}
